2aff8fbf1b0d13af58d293bc99b13c09b18a18bd
sajith <shamindasajith@gmail.com>
2023-06-23 00:15:47 +0530

    initial commit

---
+++ app/src/main/java/com/saj/mediation/Feature.kt
@@ -0,0 +1,12 @@
+package com.saj.mediation
+
+import androidx.compose.ui.graphics.Color
+
+
+data class Feature(
+    val title: String,
+    val iconId: Int,
+    val lightColor: Color,
+    val medianColor: Color,
+    val darkColor: Color
+    )
---
+++ app/src/main/java/com/saj/mediation/HomeScreen.kt
@@ -0,0 +1,143 @@
+package com.saj.mediation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.saj.mediation.ui.theme.*
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection("Shanika")
+            chipSection(listOf("Sweet Sleep", "Insomnia", "Depression", "Relax"))
+            CurrentMeditation()
+        }
+    }
+}
+
+@Composable
+fun FeatureSection(features : List<Feature>){
+
+}
+
+@Composable
+fun CurrentMeditation(color: Color = LightRed) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.h6,
+                color = Color.White
+            )
+            Text(
+                text = "Mediation 3-10 min",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .background(ButtonBlue)
+                .clip(CircleShape)
+                .padding(10.dp)
+                .size(40.dp)
+            //todo icon is not round
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play), contentDescription = "play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun chipSection(chips: List<String>) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+
+            ) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(name: String) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning, $name",
+                style = MaterialTheme.typography.h6,
+                color = Color.White
+            )
+            Text(
+                text = "We wish you have a good day!",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier
+                .size(24.dp)
+        )
+    }
+}
